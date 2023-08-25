5b2a93978c3bf5dba0f6a51568fe8302ab5efd87
johnkeshy007 <143097328+johnkeshy007@users.noreply.github.com>
2023-08-25 08:56:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_wpcdb8f", "template_va0e1ii", formData)
+    emailjs.send("service_m7y8nf6", "template_2ioslho", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
